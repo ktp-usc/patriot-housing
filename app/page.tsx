6912ab3d276dc1eee5b7fe70d1c92339cb6bc8c5
cmhanner <chanceforcode@gmail.com>
2026-03-23 18:40:46 -0400

    added bg image to project

--- app/page.tsx
+++ app/page.tsx
@@ -9,10 +9,12 @@ export default function Home() {
       <Header />
 
       <main className="mx-auto w-full max-w-7xl px-6 py-10 md:px-10 md:py-15">
-        {/* TODO: Add image/banner here */}
+
         <section className="theme-hero px-4 py-20">
-          <div className="mx-auto max-w-5xl text-center">
-            <h1 className="text-3xl font-bold md:text-5xl">Welcome to Patriot Housing</h1>
+          <div className="mx-auto max-w-5xl text-center bg-">
+            <h1 className="text-3xl font-bold md:text-5xl">
+              Welcome to Patriot Housing
+            </h1>
             <p className="theme-hero-copy mx-auto mt-5 max-w-3xl text-lg md:text-xl">
               Providing safe, affordable housing to veterans and their families, because those who served deserve a place to call home.
             </p>
@@ -24,7 +26,7 @@ export default function Home() {
             </div>
           </div>
         </section>
-        
+
         {/* TODO: Image Courasel Here, can also put in section below*/}
         <section className="rounded-2xl border border-slate-200 bg-white p-8 md:p-12">
           <h1 className="mt-3 text-3xl font-bold tracking-tight md:text-5xl">
@@ -48,7 +50,7 @@ export default function Home() {
         </section>
       </main>
 
-      <Footer />  
+      <Footer />
     </div>
   );
 }
