920126dcc75fb73f6042fcd1dcd8de8fbcb40e6e
cmhanner <chanceforcode@gmail.com>
2026-03-24 19:21:35 -0400

    add tint to bg pic-add bg to text

--- app/page.tsx
+++ app/page.tsx
@@ -10,12 +10,18 @@ export default function Home() {
 
       <main className="mx-auto w-full max-w-7xl px-6 py-10 md:px-10 md:py-15">
 
-        <section className="theme-hero px-4 py-20 bg-[url('/images/backgroundImage.jpg')] bg-cover bg-center" >
-          <div className="mx-auto max-w-5xl text-center">
-            <h1 className="text-3xl font-bold md:text-5xl">
+        <section className="relative overflow-hidden px-4 py-20 bg-[url('/images/backgroundImage.jpg')] bg-cover bg-center rounded-2xl drop-shadow-amber-100 ">
+          {/* Overlay (tint) */}
+          <div className="absolute inset-0 bg-blue-900/50"></div>
+
+          <div className="relative z-10 mx-auto max-w-3xl text-center
+                bg-white/20 backdrop-blur-md
+                px-8 py-6 rounded-2xl
+                border border-white/30 shadow-lg">
+            <h1 className="text-3xl text-white font-bold md:text-5xl drop-shadow-md ">
               Welcome to Patriot Housing
             </h1>
-            <p className="theme-hero-copy mx-auto mt-5 max-w-3xl text-lg md:text-xl">
+            <p className="theme-hero-copy mx-auto mt-5 max-w-3xl text-lg md:text-xl text-white">
               Providing safe, affordable housing to veterans and their families, because those who served deserve a place to call home.
             </p>
             <div className="mt-8 flex flex-col justify-center gap-3 sm:flex-row">
